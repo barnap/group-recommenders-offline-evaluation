1cdba8cda4e7dde1a7bb805453207e132a08d25d
amradelic <amra.delic@tuwien.ac.at>
2022-09-18 08:36:18 -0700

    Fixed some bugs with config file

--- settings/config_tourism_dataset.py
+++ settings/config_tourism_dataset.py
@@ -15,11 +15,11 @@ shared_ratings = 5
 
 # Evaluation settings
 group_types = "REAL" # "REAL" or "SYNTHETIC"
-evaluation_ground_truth = "USER_SATISFACTION" # "GROUP_CHOICES" # "USER_RATINGS"  # "USER_SATISFACTION" # "GROUP_CHOICE" (for ml-1m only possible USER_RATINGS)
+evaluation_ground_truth = "GROUP_CHOICES" # "GROUP_CHOICES" # "USER_RATINGS"  # "USER_SATISFACTION" # "GROUP_CHOICE" (for ml-1m only possible USER_RATINGS)
 group_sizes_to_test = [2,4,8]
 group_similarity_to_test = ["SIMILAR_ONE_DIVERGENT"] #["RANDOM", "SIMILAR", "DIVERGENT", "SIMILAR_ONE_DIVERGENT"]
 individual_rs_strategy = "LENSKIT_CF_ITEM"  # the used strategy for individual RS, I am keeping it generic to allow comparing more Individual Rec Sys if implemented, in a single run)
-aggregation_strategies = ["BASE", "GFAR", "EPFuzzDA"]  # ["BASE", "GFAR", "EPFuzzDA"] list of implemented aggregation strategies we want to test
+aggregation_strategies = ["BASE"]  # ["BASE", "GFAR", "EPFuzzDA"] list of implemented aggregation strategies we want to test
 recommendations_number = 5  # number of recommended items
 # recommendations_ordered = "ranking"  # sequence or ranking
 individual_rs_validation_folds_k = 0  # used for the k-fold validation)
@@ -31,7 +31,7 @@ inverse_propensity_debiasing = False #For COUPLED only: True / False whether to 
 inverse_propensity_gamma = 0.1 #gamma parameter of the inverse propensity weighting. Larger values indicate more penalization for popular items
 
 binarize_feedback = False #True / False whether to binarize user feedback for the evaluation
-binarize_feedback_positive_threshold = 4.0 # if the feedback should be binarize, this denotes the minimal positive value
+binarize_feedback_positive_threshold = 0.0 # if the feedback should be binarize, this denotes the minimal positive value
 
 feedback_polarity_debiasing = 2.0 #polarity debiasing parameter c from https://dl.acm.org/doi/10.1145/3511047.3537650 usage: rating = max(0, rating+c)
 
